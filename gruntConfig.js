e5a4c51d475d43e61f834d4ef84a4a5e6d48a255
Lars Thorup <lars@zealake.com>
2014-06-18 15:11:09 -0700

    gruntConfig

---
+++ gruntConfig.js
@@ -0,0 +1,30 @@
+/*global module*/
+module.exports = function (grunt) {
+    'use strict';
+
+    var gruntConfig = {
+        pkg: grunt.file.readJSON('package.json')
+    };
+
+    // convenience
+    grunt.registerTask('default', ['lint']);
+    grunt.registerTask('all', ['lint']);
+
+    // lint
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    gruntConfig.jshint = {
+        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
+            indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
+            quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
+            maxparams: 3, maxdepth: 2, maxstatements: 50},
+        all: [
+            'Gruntfile.js',
+            'src/**/*.js',
+            '!src/lib/**/*',
+            '!src/test/lib/**/*'
+        ]
+    };
+    grunt.registerTask('lint', 'jshint');
+
+    return gruntConfig;
+};
